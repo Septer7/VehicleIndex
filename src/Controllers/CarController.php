cdd48b8ff9ddb0e9714ba9ab32fcf7253360fcaf
Gcarlosviza <56936797+Gcarlosviza@users.noreply.github.com>
2023-05-19 14:00:51 -0400

    Update CarController.php

--- src/Controllers/CarController.php
+++ src/Controllers/CarController.php
@@ -34,35 +34,22 @@ class CarController
 
     public function getAllCars(Request $request, Response $response)
     {
-        //--step 1) get the querry params from the request
+       
         $cars = $request->getQueryParams();
         
-        //--step 3) Fetch the data from the Model
-        //new instance of the model 
+       
         $car_model = new CarModel();
 
-        //fetch a list of films
         $data= $car_model->getAll($cars);
         $json_data= json_encode($data);
 
-        //-- We need to prepare the response 
-        // $response->getBody()->write("List all the films");
         $response->getBody()->write($json_data);
 
         return $response->withStatus(201)->withHeader("Content-Type", "application/json");
     }
     public function addCar(Request $request, Response $response)
     {
-    // $params = $request->getParsedBody();
-    // $year = $params['year'];
-    // $make = $params['make'];
-    // $model = $params['model'];
     
-    // $car_model = new CarModel();
-    // $car_model->insertCar($year, $make, $model);
-    
-    // $response->getBody()->write("New car added successfully");
-    // return $response;
     $params = $request->getParsedBody();
 
     $helperFunctions = new HelperFunctions();
@@ -136,7 +123,6 @@ class CarController
 
     public function updateCar(Request $request, Response $response, array $args)
     {
-        // Get the ID of the car to update
 
 
 
@@ -147,27 +133,26 @@ class CarController
 
          
          if ($validate === true) {
-        // Get the new car data from the request body
+        
          $carData = $request->getParsedBody();
 
-        // Get an instance of the CarModel
          $carModel = new CarModel();
 
-        // Call the updateCar method in the CarModel, passing in the car ID and data
+      
          $result = $carModel->updateCar($carId, $carData);
 
-        // Check if the update was successful
+      
          if ($result === 0) {
-        // No rows were updated, so the car was not found
+       
          $message = "Car not found";
          $statusCode = 404;
          } else {
-        // The car was updated successfully
+     
          $message = "Car updated successfully";
          $statusCode = 200;
          }
 
-        // Return a JSON response with the status code and message
+       
          $response->getBody()->write(json_encode(['message' => $message]));
          return $response->withHeader('Content-Type', 'application/json')->withStatus($statusCode);
 
